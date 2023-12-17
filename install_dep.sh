f5a449028da9925648397ebfdf18ee45300213dc
NaturalSelect <2145973003@qq.com>
2023-12-17 16:47:19 +0800

    chore: remove unused packages
    Signed-off-by: NaturalSelect <2145973003@qq.com>

--- install_dep.sh
+++ install_dep.sh
@@ -7,22 +7,9 @@ apt install git -y
 apt install wget -y
 apt install libcurl4-openssl-dev -y
 apt install libsqlite3-dev -y
-apt install python3 -y
-apt install python3-pip -y
 apt install curl -y
-apt install pkg-config -y
-apt install libgl1 -y
-apt install libegl1 -y
-apt install ibxkbcommon-x11-0 -y
-apt install dbus -y
-apt install libsm-dev -y
-apt install libnss3 -y
-apt install libxcomposite1 -y
-apt install libxdamage1 -y
-apt install libxi6 libxtst6 libxrender1 -y
-apt install libasound2 -y
-apt install libxkbfile1 -y
 apt install language-pack-zh-hans -y
+apt install dbus -y
 
 # /etc/environment
 echo "LANG=\"zh_CN.UTF-8\"" >> /etc/environment
@@ -37,8 +24,3 @@ echo "zh_CN GB2312" >> /var/lib/locales/supported.d/local
 locale-gen
 
 apt install fonts-droid-fallback ttf-wqy-zenhei ttf-wqy-microhei fonts-arphic-ukai fonts-arphic-uming -y
-
-apt install ibus -y
-apt install ibus-gtk ibus-gtk3 -y
-apt install ibus-pinyin -y
-
