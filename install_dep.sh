8da10429c27942dbfc6e39cd19b8c528f63fadff
NaturalSelect <2145973003@qq.com>
2023-12-17 14:40:05 +0800

    feat: add more packages to `install_dep`
    Signed-off-by: NaturalSelect <2145973003@qq.com>

--- install_dep.sh
+++ install_dep.sh
@@ -3,3 +3,22 @@
 apt-get update
 apt-get upgrade
 apt-get install x11-xserver-utils -y
+apt install git -y
+apt install wget -y
+apt install libcurl4-openssl-dev -y
+apt install libsqlite3-dev -y
+apt install python3 -y
+apt install python3-pip -y
+apt install curl -y
+apt install pkg-config -y
+apt install libgl1 -y
+apt install libegl1 -y
+apt install ibxkbcommon-x11-0 -y
+apt install dbus -y
+apt install libsm-dev -y
+apt install libnss3 -y
+apt install libxcomposite1 -y
+apt install libxdamage1 -y
+apt install libxi6 libxtst6 libxrender1 -y
+apt install libasound2 -y
+apt install libxkbfile1 -y
